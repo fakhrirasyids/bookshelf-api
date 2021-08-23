965477a7fc12a9edf749e77e5d28757b4d3c9a96
fakhrirasyids <gomastoliq@gmail.com>
2021-08-23 07:02:12 +0700

    Innitial Commit

--- src/handler.js
+++ src/handler.js
@@ -46,6 +46,7 @@ const addBooksbyHandler = (request, h) => {
         publisher, 
         pageCount, 
         readPage, 
+        finished,
         reading,
         insertedAt,
         updatedAt,
@@ -151,23 +152,21 @@ const getBooksbyHandler = (request, h) => {
 
 const getBooksbyIdHandler = (request, h) => {
   const { id } = request.params;
+  const book = books.filter((b) => b.id === id)[0];
 
-  const book = books.filter((book) => b.id === id)[0];
-
-  if(book !== undefined) {
+  if (book !== undefined) {
     const response = h.response({
-      status: "success",
+      status: 'success',
       data: {
         book,
       },
     });
     response.code(200);
     return response;
-  };
-
+  }
   const response = h.response({
-    status: "fail",
-    message: "Buku tidak ditemukan",
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
   });
   response.code(404);
   return response;
